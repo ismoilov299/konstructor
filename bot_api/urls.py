a0ca9ca09d3eb4ead120173f9c4da2572450483b
ismoilov299 <regnad299@gmail.com>
2024-12-01 16:34:52 +0500

    fix urls

--- bot_api/urls.py
+++ bot_api/urls.py
@@ -1,11 +1,6 @@
-# bot_api/urls.py
 from django.urls import path
-
 from . import views
-from .views import telegram_webhook
-
 
 urlpatterns = [
-    # /webhook/ emas, /bot/webhook/ bo'lishi kerak
-    path('bot/webhook/<str:token>/', views.process_webhook, name='webhook'),
+    path('/webhook/<str:token>/', views.telegram_webhook, name='telegram_webhook'),#
 ]
