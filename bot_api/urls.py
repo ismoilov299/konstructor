bc6a0678aa77dacb739b116d77a7d139249e620f
ismoilov299 <regnad299@gmail.com>
2024-12-01 16:37:20 +0500

    fix url.cons

--- bot_api/urls.py
+++ bot_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/webhook/<str:token>/', views.telegram_webhook, name='telegram_webhook'),#
+    path('/webhook/<str:token>/', views.telegram_webhook, name='telegram_webhook'),
 ]
--- constructor/urls.py
+++ constructor/urls.py
@@ -3,38 +3,13 @@ from django.urls import include, path
 from django.contrib import admin
 from django.conf.urls.static import static
 
-
-from django.urls import path
-
-from bot_api import views
-
-# from . import views       .
-
-from django.conf import settings
-from django.urls import include, path
-from django.contrib import admin
-from django.conf.urls.static import static
-
 urlpatterns = [
-    path('', include('bot_api.urls')),  # bot/ prefiksini olib tashladik
-
-    path('bot/webhook/<str:token>/', views.process_webhook, name='webhook'),  # slash qo'shilgan
-
-
+    path('bot', include('bot_api.urls')),
     path('admin/', admin.site.urls),
     path('', include('modul.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns = [
-#     path('bot', include('bot_api.urls')),
-#     path('admin/', admin.site.urls),
-#     path('', include('modul.urls')),
 #    path('bot/', include('bot_api.urls'))
-# ]
-
+]
+#
 #if settings.DEBUG:
 #    import debug_toolbar
 #    urlpatterns = [
