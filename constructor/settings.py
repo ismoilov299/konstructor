81f299f6d14ff1688c68bbee3044fe819a5d8d5f
ismoilov299 <regnad299@gmail.com>
2024-12-04 22:31:10 +0500

    fix create bot html

--- constructor/settings.py
+++ constructor/settings.py
@@ -75,23 +75,23 @@ CSRF_TRUSTED_ORIGINS = ['https://test.telier.uz']
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#   'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': BASE_DIR / 'db.sqlite3',
-#   }
-#}
-
 DATABASES = {
-      'default': {
-          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-          'NAME': 'giper_db1',
-          'USER': 'giper_us1',
-          'PASSWORD': 'giper_pass1',
-          'HOST': 'localhost',
-          'PORT': 5432,
-      }
- }
+  'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': BASE_DIR / 'db.sqlite3',
+  }
+}
+
+# DATABASES = {
+#       'default': {
+#           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#           'NAME': 'giper_db1',
+#           'USER': 'giper_us1',
+#           'PASSWORD': 'giper_pass1',
+#           'HOST': 'localhost',
+#           'PORT': 5432,
+#       }
+#  }
 
 
 # Password validation
