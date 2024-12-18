79315030a345c9a3619923ef776287fbdde97eb2
ismoilov299 <regnad299@gmail.com>
2024-12-18 22:25:05 +0500

    fix main bot

--- modul/loader.py
+++ modul/loader.py
@@ -4,7 +4,7 @@ from aiogram import Router, Bot, Dispatcher
 bot_session = AiohttpSession()
 # from shazamio import Shazam
 
-main_bot = Bot(token="7400666200:AAE0q92yJUwJ-FVdAq88HK2HBjo7vOqX3_0", session=bot_session)
+main_bot = Bot(token="6746881064:AAEkmJuIEaTc8ENXZa1ntB9nwVwOqnG58gQ", session=bot_session)
 dp = Dispatcher()
 
 client_bot_router = Router()
