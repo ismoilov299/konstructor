f1b6e59d9963dcfbca8990f55c694c14c31774bc
ismoilov299 <regnad299@gmail.com>
2024-12-30 07:07:46 +0500

    admin p32

--- modul/clientbot/handlers/refs/data/excel_converter.py
+++ modul/clientbot/handlers/refs/data/excel_converter.py
@@ -12,6 +12,12 @@ import pandas as pd
 from datetime import date
 import random
 
+from asgiref.sync import sync_to_async
+from io import BytesIO
+import pandas as pd
+from datetime import date
+import random
+
 @sync_to_async
 def fetch_user_data(user_id):
 
@@ -21,12 +27,13 @@ def fetch_user_data(user_id):
     return list(users)
 
 async def convert_to_excel(user_id):
+
     try:
         users = await fetch_user_data(user_id)
 
         df = pd.DataFrame(users)
 
-        for column in df.select_dtypes(include=["datetime"]).columns:
+        for column in df.select_dtypes(include=["datetime64[ns, UTC]", "datetime64[ns]"]).columns:
             df[column] = df[column].dt.tz_localize(None)
 
         file_name = f"Referals_{user_id}_{date.today()}_{random.randint(1, 1000)}.xlsx"
