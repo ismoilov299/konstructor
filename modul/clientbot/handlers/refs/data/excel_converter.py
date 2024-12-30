90ce5fa2ac5d0166d5ee9bb2e913b73b4ccdffde
ismoilov299 <regnad299@gmail.com>
2024-12-30 07:16:19 +0500

    admin p34

--- modul/clientbot/handlers/refs/data/excel_converter.py
+++ modul/clientbot/handlers/refs/data/excel_converter.py
@@ -1,31 +1,51 @@
-from modul.models import User
 
-from tempfile import NamedTemporaryFile
-from asgiref.sync import sync_to_async
 import pandas as pd
-
+from io import BytesIO
+from datetime import datetime
+from asgiref.sync import sync_to_async
 
 @sync_to_async
-def fetch_user_data(user_id):
-    users = User.objects.filter(uid=user_id).values()
+def convert_to_excel(user_id):
+    from modul.models import UserTG
+
+    users = UserTG.objects.filter(invited_id=user_id).values(
+        'id',
+        'uid',
+        'username',
+        'balance',
+        'paid',
+        'refs',
+        'invited',
+        'invited_id',
+        'banned',
+        'created_at'
+    )
+
     if not users.exists():
-        raise ValueError("Foydalanuvchi topilmadi!")
-    return list(users)
+        raise ValueError("Kerakli ma'lumotlar topilmadi")
 
-async def convert_to_excel(user_id):
-    try:
-        users = await fetch_user_data(user_id)
-        df = pd.DataFrame(users)
+    df = pd.DataFrame(list(users))
 
-        for column in df.select_dtypes(include=["datetime64[ns, UTC]", "datetime64[ns]"]).columns:
-            df[column] = df[column].dt.tz_localize(None)
+    # Formatlash
+    df.rename(columns={
+        'id': 'user_id',
+        'uid': 'tg_id',
+        'username': 'user_name',
+        'balance': 'balance',
+        'paid': 'paid',
+        'refs': 'refs',
+        'invited': 'invited',
+        'invited_id': 'invited_id',
+        'banned': 'banned',
+        'created_at': 'reg_date'
+    }, inplace=True)
 
-        temp_file = NamedTemporaryFile(delete=False, suffix=".xlsx")
-        df.to_excel(temp_file.name, index=False)
-        return temp_file.name
+    df['reg_date'] = df['reg_date'].dt.tz_localize(None).dt.strftime('%Y-%m-%d %H:%M:%S')
 
-    except Exception as e:
-        print(f"Xato yuz berdi: {e}")
-        raise
+    excel_file = BytesIO()
+    df.to_excel(excel_file, index=False, engine='openpyxl')
+    excel_file.seek(0)
+    return excel_file
+
 
 
