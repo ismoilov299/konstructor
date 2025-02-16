3efd554d0111585151ac1d602bf0161c8fdd0d65
ismoilov299 <regnad299@gmail.com>
2025-02-17 01:19:39 +0500

    deploy 2

--- modul/bot/main_bot/main.py
+++ modul/bot/main_bot/main.py
@@ -11,7 +11,7 @@ from modul.loader import main_bot_router, client_bot_router
 import requests
 
 
-webhook_url = 'https://51fe-84-54-90-236.ngrok-free.app/login/'
+webhook_url = 'https://68cf-159-255-37-235.ngrok-free.app/login/'
 
 
 def init_bot_handlers():
