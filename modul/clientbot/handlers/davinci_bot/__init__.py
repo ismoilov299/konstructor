a9b3f0e26d964de5951d296eff5df25f4d68fb17
ismoilov299 <regnad299@gmail.com>
2025-07-01 00:20:00 +0500

    update davinchi 9

--- modul/clientbot/handlers/davinci_bot/__init__.py
+++
@@ -1,6 +0,0 @@
-from aiogram import Router
-from .handlers import davinci_router
-
-def register_davinci_handlers(router: Router):
-    """Register all davinci bot handlers"""
-    router.include_router(davinci_router)
