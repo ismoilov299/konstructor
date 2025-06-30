cb0c553641c496a5deebba649fce0089e97a8c47
ismoilov299 <regnad299@gmail.com>
2025-06-30 16:11:01 +0500

    change davinchi new version 1

--- modul/clientbot/handlers/davinci_bot/__init__.py
+++ modul/clientbot/handlers/davinci_bot/__init__.py
@@ -1,3 +1,8 @@
-from .handlers.bot import client_bot_router, davinci_install
+from .handlers.bot import client_bot_router
+
+# davinci_install funksiyasini alohida export qilamiz
+async def davinci_install():
+    """Davinci modulini o'rnatish"""
+    print("✅ Davinci module installed successfully!")
 
 __all__ = ["client_bot_router", "davinci_install"]
