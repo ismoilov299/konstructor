546ab94f90bccf68cd0927d3d7c359d9d2f7c6d4
ismoilov299 <regnad299@gmail.com>
2025-07-12 21:11:46 +0500

    change admin panel 3

--- modul/clientbot/handlers/admin/__init__.py
+++ modul/clientbot/handlers/admin/__init__.py
@@ -4,14 +4,14 @@ Universal Admin Panel Package
 Barcha bot turlari uchun universal admin panel
 """
 
-from .universal_admin import setup_universal_admin, universal_admin
+# from .universal_admin import setup_universal_admin, universal_admin
 from .handlers_extended import setup_extended_admin_handlers, extended_admin_handlers
 from .admin_service import admin_service
 from .keyboards import admin_keyboards
 from .states import AdminStates, PaymentStates, ChannelStates, SettingsStates
 
 __all__ = [
-    'setup_universal_admin',
+    # 'setup_universal_admin',
     'universal_admin',
     'setup_extended_admin_handlers',
     'extended_admin_handlers',
