068f1b070753fa988ee4b115c21b8959a19da5a8
ismoilov299 <regnad299@gmail.com>
2025-07-23 11:26:02 +0500

    correct views

--- bot_api/views.py
+++ bot_api/views.py
@@ -16,7 +16,7 @@ from modul.clientbot.handlers.admin.universal_admin import admin_panel
 # from modul.clientbot.handlers.annon_bot.handlers.admin import admin_panel
 from modul.clientbot.handlers.annon_bot.handlers.bot import anon_bot_handlers
 from modul.clientbot.handlers.chat_gpt_bot.handlers.main import chat_gpt_bot_handlers
-from modul.clientbot.handlers.davinci_bot.handlers.bot import davinchi_bot_handlers
+# from modul.clientbot.handlers.davinci_bot.handlers.bot import davinchi_bot_handlers
 # from modul.clientbot.handlers.main import start_bot_client
 # from modul.clientbot.handlers.main import start_client_bot
 
@@ -55,7 +55,7 @@ async def setup_routers():
 
             init_bot_handlers()
             anon_bot_handlers()
-            davinchi_bot_handlers()
+            # davinchi_bot_handlers()
             setup_main_bot_filter(main_bot_router, client_bot_router)
 
             # Router'larni qo'shish
