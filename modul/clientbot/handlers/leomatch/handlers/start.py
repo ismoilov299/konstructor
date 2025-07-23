e39cadc923a1085dc641e95f4997303e85958ac3
ismoilov299 <regnad299@gmail.com>
2025-07-23 12:53:33 +0500

    correct state filter 9

--- modul/clientbot/handlers/leomatch/handlers/start.py
+++ modul/clientbot/handlers/leomatch/handlers/start.py
@@ -11,34 +11,33 @@ from modul.clientbot.handlers.leomatch.handlers.shorts import manage
 # from aiogram.dispatcher.fsm.context import FSMContext
 from modul.clientbot.handlers.leomatch.data.state import LeomatchRegistration
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.utils.i18n import gettext as _
-from aiogram.utils.i18n import lazy_gettext as __
+
 
 from modul.loader import client_bot_router
 from modul.models import LeoMatchModel
 
 
-@client_bot_router.message(F.text == __("🫰 Знакомства"))
+@client_bot_router.message(F.text == ("🫰 Знакомства"))
 async def bot_start(message: types.Message, state: FSMContext):
     has_user = await exists_leo(message.from_user.id)
     if not has_user:
-        await message.answer(_("Добро пожаловать! Я - бот для знакомств. Я помогу тебе найти свою вторую половинку. "),
+        await message.answer(("Добро пожаловать! Я - бот для знакомств. Я помогу тебе найти свою вторую половинку. "),
                              reply_markup=reply_kb.begin_registration())
         await state.set_state(LeomatchRegistration.BEGIN)
     else:
         account: LeoMatchModel = await get_leo(message.from_user.id)
         if account.blocked:
-            await message.answer(_("Ваш аккаунт заблокирован"))
+            await message.answer(("Ваш аккаунт заблокирован"))
             return
         await manage(message, state)
         kbrds = InlineKeyboardBuilder()
         kbrds.add(
-            types.InlineKeyboardButton(text=_("Запустите WebApp"),
+            types.InlineKeyboardButton(text=("Запустите WebApp"),
                                        web_app=types.WebAppInfo(url='https://simplerulet.online/waleo/'))
         )
-        await message.answer(_("Попробуйте наш WebAPP знакомства"), reply_markup=kbrds.as_markup())
+        await message.answer(("Попробуйте наш WebAPP знакомства"), reply_markup=kbrds.as_markup())
 
 
-@client_bot_router.message(F.text == __("Я не хочу никого искать"), StateFilter(LeomatchRegistration.BEGIN))
+@client_bot_router.message(F.text == ("Я не хочу никого искать"), StateFilter(LeomatchRegistration.BEGIN))
 async def bot_start(message: types.Message, state: FSMContext):
     await return_main(message, state)
